e4e5905aa7f4369c5f7aea0f1fe2db99c246005b
Ananthu Pillai <ananthum0938@gmail.com>
2022-04-23 18:53:54 +0530

    Version 0.0.3
    Trying Threads with return values

---
+++ Trial and Error/threads v0.0.3.py
@@ -0,0 +1,68 @@
+import numpy as np
+from threading import Thread
+import time
+
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                        **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+def maxvalue(arr, result_in, i):
+    res = result_in[:]
+    max_value = -1
+    for k in arr:
+        if (k > max_value):
+            max_value = k
+    res[i] = max_value
+    return res[i]
+
+
+def threading(a, m, result):
+    threads = []
+    start = 0
+    end = start + int(chunk_size)
+    for i in range(m):
+        b = a[start:end]
+        process = ThreadWithReturnValue(target=maxvalue, args=[b, result, i])
+        process.start()
+        threads.append(process)
+        start = end
+        end = start + int(chunk_size)
+    i = 0
+    for process in threads:
+        result[i] = process.join()
+        i += 1
+    a = result[:]
+    return a
+
+
+a = [int((np.random.random() * 10000) % 10000) for i in range(10000000)]
+b = a[:]
+start_time = time.time()
+b.sort()
+maximum = b[-1]
+end_time = time.time()
+print(end_time - start_time)
+m = 10
+chunk_size = len(a) / m
+result = [0] * m
+
+start_time = time.time()
+a = threading(a, m, result)
+a.sort()
+print(a[-1])
+end_time = time.time()
+print(end_time - start_time)
